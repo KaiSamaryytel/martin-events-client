6d6b313881f66fb73f337337bfa6ea3975115de4
Kai Samaryytel <kai.koppel@khk.ee>
2018-09-28 10:20:24 +0300

    Adding fields and parameters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	@events = Event.where(featured: true)
   end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_102743) do
+ActiveRecord::Schema.define(version: 2018_09_28_061352) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_102743) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "featured"
   end
 
 end
