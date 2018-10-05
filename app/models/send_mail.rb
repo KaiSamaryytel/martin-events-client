557ae70c97691c09469f3f12388ac8aa763cc567
Kai Samaryytel <kai.koppel@khk.ee>
2018-10-05 14:41:20 +0300

    Fix

--- app/models/send_mail.rb
+++ app/models/send_mail.rb
@@ -6,7 +6,7 @@ class SendMail
    content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
    mail = SendGrid::Mail.new(from, subject, to, content)
   
-   sg = SendGrid::API.new(api_key: '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')
+   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
    response = sg.client.mail._('send').post(request_body: mail.to_json)
  end
 end
